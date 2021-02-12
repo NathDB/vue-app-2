a27c96c6a6f10b7649cb577d6c07ae5d0d3ba0de
Nathalie D-Bonheur <n.durassierbonheur@epsi.fr>
2021-02-12 10:31:11 +0100

    test build

---
+++ docs/precache-manifest.6024df22eb90cfc943e4f3489864cde7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2651ba0db4cc1b1347",
+    "url": "/vue-app-2/css/app.925f8dd1.css"
+  },
+  {
+    "revision": "cfecb8a314ac399a2c99",
+    "url": "/vue-app-2/css/chunk-vendors.9728716b.css"
+  },
+  {
+    "revision": "8eb16694b9b2b2594e6af193f3031e21",
+    "url": "/vue-app-2/img/logo_navbar.8eb16694.png"
+  },
+  {
+    "revision": "68958425479ed3b54771fb91638730ce",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "fc2651ba0db4cc1b1347",
+    "url": "/vue-app-2/js/app.67702ca6.js"
+  },
+  {
+    "revision": "cfecb8a314ac399a2c99",
+    "url": "/vue-app-2/js/chunk-vendors.af95169a.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
